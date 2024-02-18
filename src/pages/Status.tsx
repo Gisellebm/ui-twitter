103f55b54a6dcaadb2e1ec1578a1fc71b8984294
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2024-02-18 20:21:01 -0300

    saving new replies and tweets

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -1,16 +1,23 @@
+import { useState } from "react"
 import { Header } from "../components/Header"
 import { Separator } from "../components/Separator"
 import { Tweet } from "../components/Tweet"
 
 import './Status.css'
 
-const answers = [
-    'Concordo...',
-    'Olha, faz sentido!',
-    'Parabéns pelo progresso.'
-]
+
 
 export function Status() {
+  const [newAnswer, setNewAnswer] = useState('')
+  const [ answers, setAnswers ] = useState(['Meu primeiro tweet'])
+
+  function createNewAnswer(event: React.FormEvent) {
+    event.preventDefault()
+    
+    setAnswers([newAnswer, ...answers]) // spread operator (desestruturação de object)
+    setNewAnswer('')
+    
+  }
     return (
         <main className="status">
         <Header title='Tweet' />
@@ -19,13 +26,20 @@ export function Status() {
 
         <Separator />
 
-        <form className="answer-tweet-form">
+        <form onSubmit={createNewAnswer} className="answer-tweet-form">
           <label htmlFor="tweet">
             <img src="https://github.com/maykbrito.png" alt="Mayk Brito" />
-            <textarea id="tweet" placeholder='Tweet your answer' />
+            <textarea 
+              id="tweet" 
+              placeholder='Tweet your answer' 
+              value={newAnswer}
+              onChange={event => {
+                setNewAnswer(event.target.value)
+              }}
+            />
           </label>
 
-          <button type="submit">Answer</button>
+          <button  type="submit">Answer</button>
 
         </form>
 
--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,28 +1,43 @@
+import { useState } from "react"
 import { Header } from "../components/Header"
 import { Separator } from "../components/Separator"
 import { Tweet } from "../components/Tweet"
 import './Timeline.css'
 
-const tweets = [
-    'Meu primeiro tweet',
-    'Meu segundo tweet',
-    'Meu terceiro tweet'
-]
+
 
 // forEach x map
 
 // forEach => não tem retorno, só percorre o array
 // map => tem retorno
 
+
 export function Timeline() {
+  const [newTweet, setNewTweet] = useState('')
+  const [ tweets, setTweets ] = useState(['Meu primeiro tweet'])
+
+  function createNewTweet(event: React.FormEvent) {
+    event.preventDefault()
+    
+    setTweets([newTweet, ...tweets]) // spread operator (desestruturação de object)
+    setNewTweet('')
+    
+  }
     return (
         <main className="timeline">
           <Header title='Home' />
 
-          <form className="new-tweet-form">
+          <form onSubmit={createNewTweet} className="new-tweet-form">
             <label htmlFor="tweet">
               <img src="https://github.com/maykbrito.png" alt="Mayk Brito" />
-              <textarea id="tweet" placeholder='What`s happening' />
+              <textarea 
+                id="tweet" 
+                placeholder='What`s happening' 
+                value={newTweet}
+                onChange={event => {
+                  setNewTweet(event.target.value) 
+                }}
+              />
             </label>
 
             <button type="submit">Tweet</button>
